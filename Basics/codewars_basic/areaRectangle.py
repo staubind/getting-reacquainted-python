c43e59b6b1a10b9066a8835872c3276fbfacbe37
Dan St. Aubin <staubindj@gmail.com>
2021-10-27 21:34:05 -0500

    finished area rectangle and the longest unique set

--- Basics/codewars_basic/areaRectangle.py
+++ Basics/codewars_basic/areaRectangle.py
@@ -1,7 +1,7 @@
-from math import sqrt
-
-def area(d, l): 
-    # your code here
-    if d <= l:
-        return 'Not a rectangle'
-    return sqrt(d**2-l**2)*l
+def longest(s1, s2):
+    unique = set()
+    unique.update(list(s1))
+    unique.update(list(s2))
+    return ''.join(sorted(list(unique)))
+    
+print(longest("aretheyhere", "yestheyarehere")) # 'aehrsty'
